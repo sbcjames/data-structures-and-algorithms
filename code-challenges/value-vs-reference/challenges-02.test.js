66f9632180ca3d4fbd88de006469a0e5e677fce3
James Swift <sbcjames86@yahoo.com>
2020-09-08 22:22:08 -0700

    corrected syntax to fix fail

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -10,7 +10,7 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 
 const raisedToTheThird = (arr) => {
   let array = [];
-  for (let i=0; i< array.length; i++) {
+  for (let i=0; i < arr.length; i++) {
     array.push(Math.pow(arr[i], 3));
   }
   return array;
